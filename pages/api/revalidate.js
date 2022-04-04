ff7ac2c6c437443210c051fc3ee1b6875445478a
Walisson Lopes <walissonmarkley@gmail.com>
2022-04-03 21:27:57 -0300

    revalidate test

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,8 +1,13 @@
-export default async function handler(_req, res) {
+export default async function handler(req, res) {
+  // Check for secret to confirm this is a valid request
+
   try {
-    await res.unstable_revalidate("/get-musics");
+    await res.unstable_revalidate("/");
     return res.json({ revalidated: true });
   } catch (err) {
+    console.log(err);
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
     return res.status(500).send("Error revalidating");
   }
 }
